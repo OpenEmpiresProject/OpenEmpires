2e6e2ae0784902f6b7d72583b886d0cc1468a56b
Maleesh <maleesh.prasan@gmail.com>
2025-04-23 12:32:01 +1000

    Remove testing crash trace setup

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -16,42 +16,6 @@
 #include <stop_token>
 #include <vector>
 
-#ifdef _WIN32
-#include <dbghelp.h>
-#include <windows.h>
-
-void printStackTrace()
-{
-    constexpr int MAX_FRAMES = 64;
-    void* frames[MAX_FRAMES];
-    USHORT numFrames = CaptureStackBackTrace(0, MAX_FRAMES, frames, nullptr);
-
-    HANDLE process = GetCurrentProcess();
-    SymInitialize(process, NULL, TRUE); // Load symbol tables
-
-    SYMBOL_INFO* symbol = (SYMBOL_INFO*) malloc(sizeof(SYMBOL_INFO) + 256);
-    symbol->MaxNameLen = 255;
-    symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-
-    std::cout << "Stack trace:\n";
-    for (USHORT i = 0; i < numFrames; ++i)
-    {
-        DWORD64 address = (DWORD64) (frames[i]);
-        if (SymFromAddr(process, address, nullptr, symbol))
-        {
-            std::cout << i << ": " << symbol->Name << " at 0x" << std::hex << symbol->Address
-                      << std::dec << "\n";
-        }
-        else
-        {
-            std::cout << i << ": [symbol not found]\n";
-        }
-    }
-
-    free(symbol);
-}
-#endif
-
 using namespace aion;
 using namespace game;
 
@@ -102,34 +66,7 @@ int runGame()
     return 0;
 }
 
-// void causeCrash() {
-//     int* ptr = nullptr;
-//     *ptr = 42;
-// }
-
-extern "C" int seh_wrapper()
-{
-    __try
-    {
-        std::cout << "Starting the game\n";
-        // causeCrash();
-
-        return 1;
-        // int runGame();
-    }
-    __except (EXCEPTION_EXECUTE_HANDLER)
-    {
-        std::cout << "Caught crash via SEH.\n";
-        printStackTrace();
-        return -1;
-    }
-}
-
 int main()
 {
-    // #ifdef _WIN32
-    //     return seh_wrapper();
-    // #else
     return runGame();
-    // #endif
 }
